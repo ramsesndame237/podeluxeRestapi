6ea8c87792f9c8fc2da35dba30d7ad6e8d70cf4c
ramsesndame237 <ramsesndame34@gmail.com>
2022-12-04 18:26:35 +0100

    correction

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,26 +1,26 @@
-module.exports = {
-  HOST: "postgresql-innov.alwaysdata.net",
-  USER: "innov",
-  PASSWORD: "QVdYvinrR7i@dLA",
-  DB: "innov_podeluxe",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-  // module.exports = {
-  //   HOST: "localhost",
-  //   USER: "postgres",
-  //   PASSWORD: "root2022",
-  //   DB: "podeluxe",
-  //   dialect: "postgres",
-  //   pool: {
-  //     max: 5,
-  //     min: 0,
-  //     acquire: 30000,
-  //     idle: 10000
-  //   }
-  // };
+// module.exports = {
+//   HOST: "postgresql-innov.alwaysdata.net",
+//   USER: "innov",
+//   PASSWORD: "QVdYvinrR7i@dLA",
+//   DB: "innov_podeluxe",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+  module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "root2022",
+    DB: "podeluxe",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
